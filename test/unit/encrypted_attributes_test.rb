7488a6feb27fab95efefd43e038e1ddca17350fb
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-12-30 00:22:15 -0500

    Fix tests to be compatible with Ruby 1.9+

--- test/unit/encrypted_attributes_test.rb
+++ test/unit/encrypted_attributes_test.rb
@@ -115,25 +115,25 @@ end
 
 class EncryptedAttributesWithConditionalsTest < Test::Unit::TestCase
   def test_should_not_encrypt_if_if_conditional_is_false
-    User.encrypts :password, :if => lambda {false}
+    User.encrypts :password, :if => lambda {|user| false}
     user = create_user(:login => 'admin', :password => 'secret')
     assert_equal 'secret', user.password
   end
   
   def test_should_encrypt_if_if_conditional_is_true
-    User.encrypts :password, :if => lambda {true}
+    User.encrypts :password, :if => lambda {|user| true}
     user = create_user(:login => 'admin', :password => 'secret')
     assert_equal '8152bc582f58c854f580cb101d3182813dec4afe', "#{user.password}"
   end
   
   def test_should_not_encrypt_if_unless_conditional_is_true
-    User.encrypts :password, :unless => lambda {true}
+    User.encrypts :password, :unless => lambda {|user| true}
     user = create_user(:login => 'admin', :password => 'secret')
     assert_equal 'secret', user.password
   end
   
   def test_should_encrypt_if_unless_conditional_is_false
-    User.encrypts :password, :unless => lambda {false}
+    User.encrypts :password, :unless => lambda {|user| false}
     user = create_user(:login => 'admin', :password => 'secret')
     assert_equal '8152bc582f58c854f580cb101d3182813dec4afe', "#{user.password}"
   end
