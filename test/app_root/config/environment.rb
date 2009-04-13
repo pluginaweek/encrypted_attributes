f29d5968bc87908c64b597e9ce77d247c956abad
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2009-04-12 22:24:09 -0400

    Update tests to work with Rails 2.3

--- test/app_root/config/environment.rb
+++ test/app_root/config/environment.rb
@@ -5,4 +5,5 @@ Rails::Initializer.run do |config|
   config.plugins = %w(encrypted_strings encrypted_attributes)
   config.cache_classes = false
   config.whiny_nils = true
+  config.action_controller.session = {:key => 'rails_session', :secret => 'd229e4d22437432705ab3985d4d246'}
 end
